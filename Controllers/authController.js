83ca3fdd1d282dcaf10e81b4a6f8a2f1be1bdbdd
Manish Chitre <manish_chitre@hotmail.com>
2023-05-08 16:17:26 +0530

    added cookie options

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -11,6 +11,16 @@ createTokenSend = function (user, statusCode, res) {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+  const cookieOptions = {
+    expires : 
+      new Date( Date.now() + process.env.JWT_COOKIE_EXPFIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly : true,
+  }
+
+  if(process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie('jwt',token,cookieOptions);
+
   user.password = undefined;
 
   return res.status(statusCode).json({
